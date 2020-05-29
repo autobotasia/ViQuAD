afa174154e53dd8d399834edaf0462354f100ffa
thaithien001 <thaithien001@gmail.com>
2020-05-29 10:53:51 +0700

    update

--- flask_edittext.py
+++ flask_edittext.py
@@ -7,17 +7,12 @@ id_edit = 0
 c_id = 0
 impossible =''
 answer_start = 0
-DATABASE = 'datasets_train.db'
-database_edit = 'database_train_edit.db'
+DATABASE = 'database/database_train.db'
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
-
-def get_db_edit():
-    db_edit = sqlite3.connect(database_edit)
-    return db_edit
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -25,9 +20,6 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-    db_edit = getattr(g, '_database', None)
-    if db_edit is not None:
-        db_edit.close()
 
 @app.route('/')
 def index():
@@ -43,13 +35,11 @@ def randd():
     global answer_start
     if request.method == 'GET':
         cursor = get_db().cursor()
-        db_edit = get_db_edit()
-        cursor_edit = db_edit.cursor()
-        sqlite_select_query = """SELECT * from SqliteDb_developers"""
-        sqlite_select_query_edit = """SELECT id from SqliteDb_developers"""
-        cursor_edit.execute(sqlite_select_query_edit)
+        sqlite_select_query = """SELECT * from dataset"""
+        sqlite_select_query_edit = """SELECT dataset_id from dataset_correction"""
+        cursor.execute(sqlite_select_query_edit)
+        ids_edit = cursor.fetchall()
         cursor.execute(sqlite_select_query)
-        ids_edit = cursor_edit.fetchall()
         records = cursor.fetchall()
         i = random.randint(0,len(records)-1)
         check = []
@@ -63,7 +53,6 @@ def randd():
         c_id = int(records[i][5])
         impossible= records[i][6]
         cursor.close()
-        cursor_edit.close()
         if str(records[i][3]) == 'None':
             answer = ''
         else:
@@ -75,20 +64,18 @@ def randd():
         context_edit = None
 
     if request.method == 'POST':
-        db_edit = get_db_edit()
-        cursor_edit = db_edit.cursor()
         question_edit = request.form['question_edit']
         answer_edit = request.form['answer_edit']
         context_edit =request.form['context_edit']
-        #db_edit = get_db_edit()
-        #cursor_edit = db_edit.cursor()
-        sqlite_insert_with_param = """INSERT INTO SqliteDb_developers
-                          (id ,question, context, answer, answer_start, c_id, impossible) 
-                          VALUES (?, ?, ?, ?, ?, ?, ?)"""
-        data_tuple = (id_edit,question_edit,context_edit,answer_edit,answer_start,c_id,impossible)
-        cursor_edit.execute(sqlite_insert_with_param, data_tuple)
-        db_edit.commit()
-        cursor_edit.close()
+        db= get_db()
+        cursor = db.cursor()
+        sqlite_insert_with_param = """INSERT INTO dataset_correction
+                          (dataset_id ,question, context, answer, answer_start, c_id) 
+                          VALUES (?, ?, ?, ?, ?, ?)"""
+        data_tuple = (id_edit,question_edit,context_edit,answer_edit,answer_start,c_id)
+        cursor.execute(sqlite_insert_with_param, data_tuple)
+        db.commit()
+        cursor.close()
         return redirect(url_for('tks'))
     return render_template('edit_text.html',datatrain = datatrain, question = records[i][1], context=records[i][2], answer = answer)
     
