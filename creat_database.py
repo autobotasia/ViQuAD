066049dd23bdd99e6a517c8e63e42190c7bd5e9e
thaithien001 <thaithien001@gmail.com>
2020-05-29 11:01:34 +0700

    update

--- creat_database.py
+++ creat_database.py
@@ -1,19 +1,28 @@
 import sqlite3
 
 try:
-    sqliteConnection = sqlite3.connect('./database/database_train_edit.db')
-    sqlite_create_table_query = '''CREATE TABLE SqliteDb_developers (
+    sqliteConnection = sqlite3.connect('./database/database_train.db')
+    sqlite_create_table_query = '''CREATE TABLE dataset (
                                 id INTEGER PRIMARY KEY,
                                 question TEXT NOT NULL,
                                 context TEXT NOT NULL,
                                 answer TEXT,
                                 answer_start INTEGER,
                                 c_id INTEGER,
-                                impossible TEXT);'''
+                                impossible TEXT)'''
+    sqlite_create_table_query_correct = '''CREATE TABLE dataset_correction (
+                                dataset_id INTEGER PRIMARY KEY,
+                                question TEXT NOT NULL,
+                                context TEXT NOT NULL,
+                                answer TEXT,
+                                answer_start INTEGER,
+                                c_id INTEGER)'''
 
     cursor = sqliteConnection.cursor()
     print("Successfully Connected to SQLite")
-    cursor.execute(sqlite_create_table_query)
+    #cursor.execute(sqlite_create_table_query)
+    #sqliteConnection.commit()
+    cursor.execute(sqlite_create_table_query_correct)
     sqliteConnection.commit()
     print("SQLite table created")
 
